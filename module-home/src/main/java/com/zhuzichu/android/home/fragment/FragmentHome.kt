24b7a96c83b6f6ec88697595da17bc64bfdf5f2f
zhuzichu520 <zhuzichu520@gmail.com>
2020-10-16 10:21:36 +0800

    update home

--- module-home/src/main/java/com/zhuzichu/android/home/fragment/FragmentHome.kt
+++ module-home/src/main/java/com/zhuzichu/android/home/fragment/FragmentHome.kt
@@ -1,12 +1,11 @@
 package com.zhuzichu.android.home.fragment
 
-import androidx.lifecycle.Observer
 import com.alibaba.android.arouter.facade.annotation.Route
-import com.hiwitech.android.libs.internal.MainHandler
 import com.hiwitech.android.mvvm.base.ArgDefault
+import com.qmuiteam.qmui.recyclerView.QMUIRVDraggableScrollBar
 import com.qmuiteam.qmui.widget.pullLayout.QMUIPullLayout
-import com.zhuzichu.android.home.R
 import com.zhuzichu.android.home.BR
+import com.zhuzichu.android.home.R
 import com.zhuzichu.android.home.databinding.FragmentHomeBinding
 import com.zhuzichu.android.home.viewmodel.ViewModelHome
 import com.zhuzichu.android.shared.base.FragmentBase
@@ -19,6 +18,13 @@ class FragmentHome : FragmentBase<FragmentHomeBinding, ViewModelHome, ArgDefault
     override fun bindVariableId(): Int = BR.viewModel
 
     override fun setLayoutId(): Int = R.layout.fragment_home
+
+    override fun initView() {
+        super.initView()
+        val scrollBar = QMUIRVDraggableScrollBar(0, 0, 0)
+        scrollBar.isEnableScrollBarFadeInOut = true
+        scrollBar.attachToRecyclerView(recycler)
+    }
 
     override fun initData() {
         super.initData()
